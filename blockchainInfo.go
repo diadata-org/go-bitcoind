9dedf42af7c3cfa8d9547035f584831cc6f29ca3
Franck <franck@mumblecore.org>
2018-08-20 11:45:57 +0200

    Adding Mediantime

--- blockchainInfo.go
+++ blockchainInfo.go
@@ -1,6 +1,7 @@
 package bitcoind
 
 type BlockchainInfo struct {
-	Blocks float64 `json:"blocks"`
-	Initialblockdownload bool `json:"initialblockdownload"`
+	Blocks               float64 `json:"blocks"`
+	Initialblockdownload bool    `json:"initialblockdownload"`
+	Mediantime           int64   `json:"mediantime"`
 }
